e19487680bc516f05177aa960835e0d06b0ec1df
Hari Shekhar <hari.shekhar@landmarkgroup.in>
2018-11-11 16:07:20 +0530

    first commit

---
+++ build/precache-manifest.068a9f539265167539b479f3de4b6789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9598e7512654804e1476",
+    "url": "/static/js/main.9598e751.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "9598e7512654804e1476",
+    "url": "/static/css/main.bbb129de.chunk.css"
+  },
+  {
+    "revision": "16d57a9ee1b41161eec21f50ab80ffcb",
+    "url": "/index.html"
+  }
+];
